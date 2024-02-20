643e1854f1f5e30e2b3bdf46d7d923a87300a2f0
UrsaMajor-t <changsheng010909@gmail.com>
2024-02-20 10:09:15 +0800

    update

--- src/ecdsa/gadgets/ecdsa.rs
+++ src/ecdsa/gadgets/ecdsa.rs
@@ -82,7 +82,7 @@ pub struct ECDSASignatureTarget<C: Curve> {
 }
 
 const ECDSA_BATCH_SIZE: usize = 20;
-const PROVE_RUN_TIMES: usize = 1;
+const PROVE_RUN_TIMES: usize = 20;
 
 pub struct CustomGateSerializer;
 
@@ -444,7 +444,8 @@ pub fn test_batch_ecdsa_cuda_circuit_with_config(batch_num: usize, config: Circu
 
     let path_string = "data/data_cuda_".to_string() + &batch_num.to_string() + "_bytes_" + last;
     let path = std::path::Path::new(&path_string);
-    let data = if path.exists() {
+    let generators_cache_path = "data/idx_dependencies_map.txt".to_string();
+    let mut data = if path.exists() {
         let start_timer = std::time::Instant::now();
         println!("Reading data");
         let circuit_data_bytes = std::fs::read(path).unwrap();
@@ -471,6 +472,7 @@ pub fn test_batch_ecdsa_cuda_circuit_with_config(batch_num: usize, config: Circu
 
     for j in 0..PROVE_RUN_TIMES {
         //let j = std::hint::black_box(j);
+        data.load_generators_cache(generators_cache_path.clone());
         let (msg_list, sig_list, pk_list) = gen_batch_ecdsa_data(batch_num);
 
         let mut pw = PartialWitness::new();
@@ -493,11 +495,11 @@ pub fn test_batch_ecdsa_cuda_circuit_with_config(batch_num: usize, config: Circu
 
         //let proof = std::hint::black_box(data.prove(pw).unwrap());
         // normal test
-        let proof = data.prove(pw).unwrap();
+        // let proof = data.prove(pw).unwrap();
         // test out pinned memory
         // let proof = data.prove_with_out_pinned_memory(pw).unwrap();
         // test out memory
-        //let proof = data.prove_with_out_memory(pw).unwrap();
+        let proof = data.prove_with_out_memory(pw).unwrap();
         //print!("{j}th prove costs time :{:?}",prove_start_time.elapsed() );
 
         println!("proof PIS {:?}", proof.public_inputs);
